cad092c3c6f8d53801a4a6b0a65fd226ff13742e
Lior <diler.lior@gmail.com>
2021-11-16 12:35:06 +0100

    initial commit

---
+++ Assets/test.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using SteamAudio;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    private SteamAudioSource _steamAudioSource;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
