af0a36a99faa6c35136fbfb48b93430884837966
Lior <diler.lior@gmail.com>
2021-12-26 22:47:50 +0100

    untested velocity change movement

--- Assets/Scripts/Fly.cs
+++ Assets/Scripts/Fly.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,12 +9,22 @@ public class Fly : MonoBehaviour
 {
 
     public GameObject flyObject;
+    public GameObject leftController;
     public Transform playerTransform;
     
     public float distance = 5f;
-
+    public float radius = 2f;
+    
     private Vector3 _playerPosition;
 
+    
+    private float _distance;
+    private Vector3 flyPos;
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(flyObject.transform.position, radius);
+    }
 
     // Start is called before the first frame update
     void Start()
@@ -24,14 +35,26 @@ public class Fly : MonoBehaviour
 
     public void MoveFly()
     {
-        flyObject.transform.RotateAround(_playerPosition, new Vector3(0, 1, 0), 50 * Time.deltaTime);
+        flyPos = flyObject.transform.position;
+        //flyObject.transform.RotateAround(_playerPosition, new Vector3(0, 1, 0), 50 * Time.deltaTime);
+        Vector3 vec = leftController.GetComponent<Rigidbody>().velocity;
+        flyObject.GetComponent<Rigidbody>().velocity = vec * -1;
     }
 
     // Update is called once per frame
-    /*void Update()
+    void Update()
     {
-        MoveFly();
+        _distance = Vector3.Distance(leftController.transform.position, flyObject.transform.position);
+
+        if (_distance < radius)
+        {
+            MoveFly();
+        }
+        else if (Vector3.Distance(flyObject.transform.position, flyPos) > 2)
+        {
+            flyObject.transform.position = flyPos;
+        }
 
         //leftController.TryGetFeatureValue(CommonUsages.menuButton, out bool returnToLobby);
-    }*/
+    }
 }
