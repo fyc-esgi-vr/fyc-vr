a5f930f96817e4afe1cbb70fca20fa358e5a2abf
Lior <diler.lior@gmail.com>
2022-01-03 04:13:56 +0100

    untested fix grab interacting for fly

--- Assets/Scripts/FollowFly.cs
+++ Assets/Scripts/FollowFly.cs
@@ -6,20 +6,33 @@ public class FollowFly : MonoBehaviour
 {
     public GameObject fly;
     public Vector3 offset;
+    
+    private bool _flyIsMoving;
+    private Vector3 _sizeFactor;
     private Transform _transform;
     private void Start()
     {
-        _transform = transform;
-        Vector3 pos = transform.localPosition;
-        pos += offset;
-        _transform.localPosition = pos;
-        _transform.localScale = Vector3.one * 0.5f;
+        _transform = this.gameObject.transform;
+        var followFlyScale = _transform.localScale;
+        var flyScale = fly.transform.localScale;
+        float x = followFlyScale.x / flyScale.x;
+        float y = followFlyScale.y / flyScale.y;
+        float z = followFlyScale.z / flyScale.z;
+
+        _sizeFactor = new Vector3(x, y, z);
+    }
+
+    public void SetFlyMoving(bool val)
+    {
+        _flyIsMoving = val;
     }
          
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        _transform.localPosition = fly.transform.localPosition;
+        //if (!_flyIsMoving) return;
+        var flyTransform = fly.transform.localPosition;
+        _transform.localPosition = new Vector3(flyTransform.x * 3,flyTransform.y,(flyTransform.z* (3)) - 0.9f);
         _transform.localRotation = fly.transform.localRotation;
     }
 }
