ef8021abf42307e7162335367718e6e6c8f29434
Lior <diler.lior@gmail.com>
2022-01-03 22:30:15 +0100

    untested fly movement with dimension box

--- Assets/Scripts/FollowFly.cs
+++ Assets/Scripts/FollowFly.cs
@@ -4,17 +4,52 @@ using UnityEngine;
 
 public class FollowFly : MonoBehaviour
 {
+    public float realDimensionSize = 3;
+    public float littleDimensionSize = 1;
+
+    public GameObject flySoundObject;
     //object to track
-    public GameObject fly;
+    public GameObject flyPrefab;
+    public GameObject characterModelPrefab;
+
+    private GameObject _fly;
+    public Vector3 littleDimensionBotLeftBackPoint;
+    public Vector3 realDimensionBotLeftBackPoint;
     
     private bool _flyIsMoving;
-    private Transform _transform;
-    //offset so that Z axis is lined up with character
-    private float _zOffset;
-    private void Start()
+    private float _proportion;
+    private Vector3 lastValidFlyPos;
+    void OnDrawGizmosSelected()
     {
-        _transform = this.gameObject.transform;
-        _zOffset = fly.transform.position.z;
+        // Draw a semitransparent blue cube at the transforms position
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        var centerLittleDimension = new Vector3(littleDimensionBotLeftBackPoint.x + littleDimensionSize / 2,
+            littleDimensionBotLeftBackPoint.y + littleDimensionSize / 2,
+            littleDimensionBotLeftBackPoint.z + littleDimensionSize / 2);
+        Gizmos.DrawCube(centerLittleDimension, new Vector3(littleDimensionSize, littleDimensionSize, littleDimensionSize));
+    }
+
+    private void Awake()
+    {
+        var centerLittleDimension = new Vector3(littleDimensionBotLeftBackPoint.x + littleDimensionSize / 2,
+            littleDimensionBotLeftBackPoint.y + littleDimensionSize / 2,
+            littleDimensionBotLeftBackPoint.z + littleDimensionSize / 2);
+        Mesh m = characterModelPrefab.GetComponent<MeshFilter>().sharedMesh;
+        Bounds meshBounds = m.bounds;
+        Vector3 meshSize = meshBounds.size;
+        characterModelPrefab.transform.localScale = new Vector3(littleDimensionSize / meshSize.x * 0.3f,littleDimensionSize / meshSize.y * 0.5f,littleDimensionSize / meshSize.z * 0.3f);
+        Instantiate(characterModelPrefab, centerLittleDimension, Quaternion.identity);
+        
+        m = flyPrefab.GetComponent<MeshFilter>().sharedMesh;
+        meshBounds = m.bounds;
+        meshSize = meshBounds.size;
+        flyPrefab.transform.localScale = new Vector3(littleDimensionSize / meshSize.x * 0.1f,littleDimensionSize / meshSize.y* 0.1f,littleDimensionSize / meshSize.z* 0.1f);
+        var flyPos = new Vector3(littleDimensionBotLeftBackPoint.x + littleDimensionSize / 4,
+            littleDimensionBotLeftBackPoint.y + littleDimensionSize / 2,
+            littleDimensionBotLeftBackPoint.z + littleDimensionSize / 1.25f);
+        _fly = Instantiate(flyPrefab, flyPos, Quaternion.identity);
+        _proportion = realDimensionSize / littleDimensionSize;
+        Debug.Log(_proportion);
     }
 
     //called when the player is grabbing the fly
@@ -23,13 +58,37 @@ public class FollowFly : MonoBehaviour
     {
         _flyIsMoving = val;
     }
-         
+
+    private bool VerifyFlyPosition(Vector3 pos)
+    {
+        if (pos.x < littleDimensionBotLeftBackPoint.x ||
+            pos.x > littleDimensionBotLeftBackPoint.x + littleDimensionSize)
+            return false;
+        if (pos.y < littleDimensionBotLeftBackPoint.y ||
+            pos.y > littleDimensionBotLeftBackPoint.y + littleDimensionSize)
+            return false;
+        if (pos.z < littleDimensionBotLeftBackPoint.z ||
+            pos.z > littleDimensionBotLeftBackPoint.z + littleDimensionSize)
+            return false;
+        return true;
+    }
     // Update is called once per frame
     private void Update()
     {
-        if (!_flyIsMoving) return;
-        var flyTransform = fly.transform.localPosition;
-        _transform.localPosition = new Vector3(flyTransform.x * 3,flyTransform.y,flyTransform.z - _zOffset);
-        _transform.localRotation = fly.transform.localRotation;
+        //if (!_flyIsMoving) return;
+        var flyPos = _fly.transform.position;
+        if (VerifyFlyPosition(flyPos))
+            lastValidFlyPos = flyPos;
+        else
+            _fly.transform.position = lastValidFlyPos;
+        
+        var difference = new Vector3(flyPos.x - littleDimensionBotLeftBackPoint.x,
+            flyPos.y - littleDimensionBotLeftBackPoint.y, 
+            flyPos.z - littleDimensionBotLeftBackPoint.z);
+        flySoundObject.transform.position = new Vector3(realDimensionBotLeftBackPoint.x + difference.x * _proportion,
+            realDimensionBotLeftBackPoint.y + difference.y * _proportion,
+            realDimensionBotLeftBackPoint.z + difference.z * _proportion);
+        flySoundObject.transform.rotation = _fly.transform.rotation;
+        
     }
 }
